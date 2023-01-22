11f1791e9752ecab7f0830f636df78f0668de43b
Yury Sychyk <kauz@tut.by>
2023-01-22 22:30:10 +0300

    2

--- Tuples.playground/Contents.swift
+++ Tuples.playground/Contents.swift
@@ -2,3 +2,9 @@ import UIKit
 
 var greeting = "Hello, playground"
 var tupl = (1, "hello", true)
+//var tupl1 (numberOne, name2, cfu) = tupl
+tupl.0
+tupl.1
+tupl.2
+let ( numberT, nameT, BoolT) = tupl
+print(tupl)
